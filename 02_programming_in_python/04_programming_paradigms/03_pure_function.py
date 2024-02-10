41d7ac3f209b6c4ea8db96541e1c87b4f445f99f
Maykal Tenev <michaeltenev@gmail.com>
2024-02-10 23:55:16 +0100

    pure functions

---
+++ 02_programming_in_python/04_programming_paradigms/03_pure_function.py
@@ -0,0 +1,19 @@
+# def add_to_list(lst, item):
+#     nl = lst.copy()
+#     nl.append(item)
+
+#     return nl
+
+my_list = [1, 2, 3]
+
+
+def add_to_list(list, item):
+    nl = list.copy()
+    nl.append(item)
+    return nl
+
+
+new_list = add_to_list(my_list, 4)
+
+print(new_list)
+print(my_list)
